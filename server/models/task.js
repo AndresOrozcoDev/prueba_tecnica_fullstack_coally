d47f4282643fa68661953e42d95eebe0260326c6
Andres Orozco <andres.orozco.dev@gmail.com>
2024-12-29 22:46:24 -0500

    remove key table

--- server/models/task.js
+++ server/models/task.js
@@ -4,6 +4,6 @@ const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String },
     completed: { type: Boolean, default: false },
-}, { timestamps: { createdAt: true, updatedAt: false } });
+}, { timestamps: { createdAt: true, updatedAt: false }, versionKey: false });
 
 module.exports = mongoose.model('Task', taskSchema);
