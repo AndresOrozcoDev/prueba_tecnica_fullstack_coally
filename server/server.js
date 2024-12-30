48da7294a2c56d14a659bcbed38835ac10ef6069
Andres Orozco <andres.orozco.dev@gmail.com>
2024-12-30 12:21:19 -0500

    add cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
 const swaggerDocs = require("./swagger");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
-const taskRoutes = require('./routes/taskRoutes');
-const connectToDatabase = require('./config/database');
+const taskRoutes = require("./routes/taskRoutes");
+const connectToDatabase = require("./config/database");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,9 +15,13 @@ connectToDatabase();
 
 // Configuración de CORS
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://prueba-tecnica-fullstack-coally.onrender.com/"],
+  origin: [
+    "http://localhost:5173",
+    "https://prueba-tecnica-fullstack-coally.onrender.com",
+    "https://sunny-souffle-83db73.netlify.app",
+  ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: ["Content-Type"]
+  allowedHeaders: ["Content-Type"],
 };
 
 // Middlewares
@@ -28,11 +32,13 @@ app.use(bodyParser.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
-app.use('/api/tasks', taskRoutes);
+app.use("/api/tasks", taskRoutes);
 
 // Iniciamos el servidor
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Swagger docs available at http://localhost:${PORT}/docs`);
-  console.log(`OpenAPI JSON available at http://localhost:${PORT}/openapi.json`);
+  console.log(
+    `OpenAPI JSON available at http://localhost:${PORT}/openapi.json`
+  );
 });
